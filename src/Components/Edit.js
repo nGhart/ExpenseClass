0cf8195571d4dd7e0517ee8b62060be815f2429f
derek-atiawu <122231182+derek-atiawu@users.noreply.github.com>
2023-06-19 07:35:17 +0000

    Update Edit.js
    included the form interface

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -2,7 +2,148 @@ import React, {Component} from 'react';
 
 class Edit extends Component{
 return(
-  <div>Edit Interface</div>
+  <div>
+  <Form
+          style={{
+            width: '100%',
+            minHeight: '300px',
+            margin: '5px',
+            padding: '10px',
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form.Group
+            className="mb-3"
+            controlId="formBasicDate"
+            style={{
+              height: '15%',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <Form.Label
+              style={{
+                width: '30%',
+              }}
+            >
+              Date
+            </Form.Label>
+            <Form.Control
+              type="date"
+              name="date"
+              value={state.date}
+              onChange={handleChange}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="formBasicExpense"
+            style={{
+              height: '15%',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <Form.Label
+              style={{
+                width: '30%',
+              }}
+            >
+              Expense
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Expense"
+              maxLength={20}
+              name="name"
+              value={state.name}
+              onChange={handleChange}
+              required
+            />
+          </Form.Group>
+
+          <Form.Group
+            className="mb-3"
+            controlId="exampleFormCategory"
+            style={{
+              height: '15%',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <Form.Label
+              style={{
+                width: '30%',
+              }}
+            >
+              Category
+            </Form.Label>
+            <Form.Select
+              aria-label="Default select example"
+              type="text"
+              onChange={handleChange}
+              name="category"
+              value={state.category}
+            >
+              <option> Choose Category</option>
+              <option value="Food">Food</option>
+              <option value="Transport">Transport</option>
+<option value="Groceries">Groceries</option>
+<option value="Utilities">Utilities</option>
+<option value="Rent">Rent</option>
+<option value="Self-care">Self-care</option>
+              <option value="Entertainment">Entertainment</option>
+<option value='Miscellaneous'>Miscellaneous</option>
+            </Form.Select>
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="exampleForm.ControlTextarea1"
+            style={{
+              height: '15%',
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+            }}
+          >
+            <Form.Label
+              style={{
+                width: '30%',
+              }}
+            >
+              Amount
+            </Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Enter Amount"
+              onChange={handleChange}
+              name="amount"
+              value={state.amount}
+              required
+            />
+          </Form.Group>
+          <Form.Group
+            style={{
+              height: '100%',
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-around',
+              alignItems: 'center',
+              border: 'none',
+            }}
+          >
+            <Button
+              type="submit"
+              style={{ width: '150px', backgroundColor: 'cornflowerblue' }}
+            >
+              <span>Add</span>
+            </Button>
+          </Form.Group>
+        </Form>
+  </div>
 );
 
 export default Edit;
