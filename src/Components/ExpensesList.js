5e32ea6c78aeaa98c7e6f29bf062986b12a666ae
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-21 13:42:43 +0000

    Update ExpensesList.js

--- src/Components/ExpensesList.js
+++ src/Components/ExpensesList.js
@@ -4,14 +4,13 @@ import SingleExpense from './SingleExpense';
 import Stack from 'react-bootstrap/Stack';
 
 const ExpensesList = () => {
-  const state = useSelector((state) => {
-    return state.userReducer;
-  });
+  const state = useSelector((state) => state.userReducer);
+  console.log(state);
   return (
     <>
       <Stack gap={2} style={{ marginTop: '5px' }}>
         {state.expenses.map((item) => {
-          return <SingleExpense key={item.id} item={item} />;
+          return <SingleExpense key={item.id} user={item} />;
         })}
       </Stack>
     </>
