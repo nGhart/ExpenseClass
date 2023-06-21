cf5931eda1d54890f33700310cb38e99f4f8a442
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-21 14:47:01 +0000

    Update Edit.js

--- src/Components/Edit.js
+++ src/Components/Edit.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { editExpense } from '../features/appSlice';
